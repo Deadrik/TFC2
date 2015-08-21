799937960a2e126f1d61a0f38f4c9a447a8f70c6
Bioxx <bioxx2007@gmail.com>
2015-08-21 09:41:29 -0400

    Offshoot caves have a slightly bigger minimum size now to make sure that they appear correctly.

--- src/API/com/bioxx/jmapgen/processing/CaveProcessor.java
+++ src/API/com/bioxx/jmapgen/processing/CaveProcessor.java
@@ -162,8 +162,8 @@ public class CaveProcessor
 					sCurNode = new CaveAttrNode(1000+subCaveCount);
 					sCurNode.setOffset(curNode.getOffset());
 					sCurNode.setNext(sNextCenter);
-					sCurNode.setNodeHeight(1+map.mapRandom.nextInt(2));
-					sCurNode.setNodeWidth(1+map.mapRandom.nextInt(2));
+					sCurNode.setNodeHeight(1+map.mapRandom.nextInt(3));
+					sCurNode.setNodeWidth(2+map.mapRandom.nextInt(2));
 					sNextNode = new CaveAttrNode(1000+subCaveCount);
 					sNextNode.setOffset(new BlockPos(sNextCenter.point.x, sCurNode.getOffset().getY()+map.mapRandom.nextInt(20)-10, sNextCenter.point.y));
 					sCurNode.setNextOffset(getMidpoint(sCurNode.getOffset(), sNextNode.getOffset()).add(10-map.mapRandom.nextInt(6), 10-map.mapRandom.nextInt(6), 10-map.mapRandom.nextInt(6)));
