4b121f7d87bc68e7eae36d6c94aecaa34abbfab5
Bioxx <bioxx2007@gmail.com>
2016-01-06 21:44:01 -0500

    Added TileTFC as a base class for all future Tile Entities. This class splits reading and writing nbt into two methods each to allow syncing only data that actually needs to be synced instead of all of the nbt data for this block.

---
+++ src/Common/com/bioxx/tfc2/tileentities/TileTFC.java
@@ -0,0 +1,65 @@
+package com.bioxx.tfc2.tileentities;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
+import net.minecraft.tileentity.TileEntity;
+
+public class TileTFC extends TileEntity 
+{
+	@Override
+	@Deprecated
+	public void readFromNBT(NBTTagCompound compound)
+	{
+		super.readFromNBT(compound);
+		readSyncableNBT(compound);
+		readNonSyncableNBT(compound);
+	}
+
+	public void readSyncableNBT(NBTTagCompound compound)
+	{
+
+	}
+
+	public void readNonSyncableNBT(NBTTagCompound compound)
+	{
+
+	}
+
+	@Override
+	@Deprecated
+	public void writeToNBT(NBTTagCompound compound)
+	{
+		super.writeToNBT(compound);
+		writeSyncableNBT(compound);
+		writeNonSyncableNBT(compound);
+	}
+
+	public void writeSyncableNBT(NBTTagCompound compound)
+	{
+
+	}
+
+	public void writeNonSyncableNBT(NBTTagCompound compound)
+	{
+
+	}
+
+	@Override
+	public void onDataPacket(net.minecraft.network.NetworkManager net, net.minecraft.network.play.server.S35PacketUpdateTileEntity pkt)
+	{
+		this.readSyncableNBT(pkt.getNbtCompound());
+	}
+
+	/**
+	 * Allows for a specialized description packet to be created. This is often used to sync tile entity data from the
+	 * server to the client easily. For example this is used by signs to synchronise the text to be displayed.
+	 */
+	@Override
+	public Packet getDescriptionPacket()
+	{
+		NBTTagCompound nbt = new NBTTagCompound();
+		this.writeSyncableNBT(nbt);
+		return new S35PacketUpdateTileEntity(this.pos, this.getBlockMetadata(), nbt);
+	}
+}
